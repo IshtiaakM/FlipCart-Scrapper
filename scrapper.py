68ceed1e15d886bbdb4459021cfafd961d18db9d
nihal <nihal@nihals-MacBook-Air.local>
2025-06-25 21:45:23 +0600

    integrated BeautifulSoup to extract and print phone names from Flipkart search results

--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,6 @@
 import asyncio
 from playwright.async_api import async_playwright
+from bs4 import BeautifulSoup
 
 
 async def main(query="smartphones", max_pages= int(input("enter how many page you want to scrap >  "))):
@@ -13,16 +14,23 @@ async def main(query="smartphones", max_pages= int(input("enter how many page yo
 
 
         await page.wait_for_selector('div.cPHDOP.col-12-12')  
-        contents = []
-        contents.append(await page.content())
+        contents = await page.content()
        
         for i in range(1, max_pages+1):
+            soup = BeautifulSoup(contents, 'lxml')
+            all_item_details = soup.find_all('div', class_ = 'cPHDOP.col-12-12')
+
+
+            for item in all_item_details:
+                phone_name = item.find('div', class_ = "KzDlHZ").text
+                print(phone_name)
            
             await page.goto(f"https://www.flipkart.com/search?q={query}&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off&page={i}")
-            contents.append(await page.content())
+            contents = await page.content()
 
 
-        print(contents)
+
+
 
 
 asyncio.run(main())
